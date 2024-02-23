0af8375cdb0d46b6ee00d617fcae48c6e8a75eaf
SurajSai <132142902+SurajSia@users.noreply.github.com>
2024-02-23 16:39:54 +0530

    Add files via upload

---
+++ Caesar.py
@@ -0,0 +1,18 @@
+
+def Caesar(text,shift):
+    alphabet='abcdefghijklmnopqrstuvwxyz'
+    encrypt_txt=''
+
+    for char in text.lower():
+        if char == ' ':
+            encrypt_txt+=char
+        else:
+            index=alphabet.find(char)
+            n_index=index+shift
+            encrypt_txt+=alphabet[n_index]
+        print('Plain-Text : ',text)
+        print('encrypted-text : ',encrypt_txt)
+
+text=input("Enter your Plain-text : ")
+shift=int(input("Enter you shift-value : "))
+Caesar(text,shift)
