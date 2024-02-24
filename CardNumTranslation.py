af8c89864c3f37af2ed0c26c6d6af82793f6e721
SurajSai <132142902+SurajSia@users.noreply.github.com>
2024-02-24 11:10:18 +0530

    Update CardNumTranslation.py

--- CardNumTranslation.py
+++ CardNumTranslation.py
@@ -1,4 +1,5 @@
-def verify_card_number(card_number):
+#Credit/Debit Card Number Validation using numbers and string concepts with python programming
+def verify_card_number(card_number): # function to validate the given number
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
@@ -17,12 +18,12 @@ def verify_card_number(card_number):
     print(total)
     return total % 10 == 0
 
-def main():
+def main():                  # main function to read the users input , calling verifty_card_number function
     card_number = '4111-1111-4555-1141'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
-    if verify_card_number(translated_card_number):
+    if verify_card_number(translated_card_number): # checks the validity and indvalidity by calling verifty_card_number function
         print('VALID!')
     else:
         print('INVALID!')
