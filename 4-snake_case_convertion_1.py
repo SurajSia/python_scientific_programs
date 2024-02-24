f833c57da7b74c0f82d365fadc6452d50183b7dd
SurajSai <132142902+SurajSia@users.noreply.github.com>
2024-02-24 19:37:31 +0530

    Update 4-snake_case_convertion_1.py

--- 4-snake_case_convertion_1.py
+++ 4-snake_case_convertion_1.py
@@ -1,24 +1,28 @@
-# this python is used to take the pascal or camel string as an input and the
-# output will be the form of a clean snake case program
-def convert_to_snake_case(pascal_or_camel_cased_string):
+#Python Program to convert a string in pascal or camel case into a clean snake case using string manipulation
 
-    for char in pascal_or_camel_cased_string:
-        if char.isupper():
-            converted_char= '_' + char.lower()
-            snake_char_list.append(converted_char)
-        else:
-            snake_char_list.append(char)
-    #snake_case_string=''.join(snake_char_list)
-    #clean_snake_case_string=snake_case_string.strip('_')
-    #return clean_snake_case_string
+#MIT License Board
+#copyright(cc) @ GudalaSaiSuraj 
+#Authorized By freecodecamp.org
+
+def convert_to_snake_case(pascal_or_camel_case):
     
-
-    return ''.join(snake_cased_char_list).strip('_')
+    snake_case_list = []    # declaring an empty list
+    
+    for char in pascal_or_camel_case:
+        if char.isupper():
+            
+            converted_char='_' + char.lower()
+            snake_case_list.append(converted_char)
+        else:
+            snake_case_list.append(char)
+    
+    return ''.join(snake_case_list).strip('_')
 
 def main():
-    print(convert_to_snake_case('aLongAndComplexString'))
-
     
+    string = 'aLongStringForExample'   # input was given in pascal-case i.e.;
+                                       # needed to converted using fucntion call
+    print(convert_to_snake_case(string))    
 
 if __name__ == '__main__':
     main()
